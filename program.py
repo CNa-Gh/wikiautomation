1c1396a84a0d5aeabd6ed03aaa28f86135aa2d9e
CNa-Gh <seenagh@gmail.com>
2020-11-20 21:48:33 -0500

    added option to click on medes island and check country

--- program.py
+++ program.py
@@ -1,20 +1,16 @@
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 import time
-
-
-driver = webdriver.Chrome()
+driver = webdriver.Chrome("chromedriver.exe")
 driver.get("https://www.wikipedia.org/")
-
-
-english = driver.find_element_by_id("js-link-box-en")
-english.click()
 search = driver.find_element_by_id("searchInput")
 search.send_keys("medes")
-search.send_keys(Keys.DOWN)
-search.send_keys(Keys.RETURN)
-
-
-
-time.sleep(100)
+time.sleep(1)
+for element in driver.find_elements_by_xpath("//*[@id='typeahead-suggestions']/div/a/div[1]/h3"):
+    if element.text.lower() == "medes islands":
+        element.click()
+        break
+time.sleep(2)
+spain = driver.find_element_by_xpath("//*[@id='mw-content-text']/div[1]/table[1]/tbody/tr[6]/td/a")
+print(spain.get_attribute("href"))
 driver.close()
